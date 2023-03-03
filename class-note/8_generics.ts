0911b79496f9c6e9b748fe3e03d14c3e9cb8fd2a
Hyeyeon(Anne) <khy2106@naver.com>
2023-03-03 15:34:12 +0900

    [TIL] generic type

---
+++ class-note/8_generics.ts
@@ -0,0 +1,74 @@
+// function logText(text) {
+//     console.log(text);
+//     return text;
+// }
+
+// logText(10);
+// logText('하이');
+// logText(true);
+
+
+function logText<T>(text: T):T {
+    console.log(text);
+    return text;
+}
+logText<string>('하이');
+
+
+// 인터페이스에 제네릭을 선언하는 방법
+interface DropdownExample {
+    value: string; // value 값은 언제든 바뀔 수 있음.
+    selected: boolean;
+}
+
+const object: DropdownExample = {
+    value: 'abc',  
+    selected: false
+}
+
+
+interface Dropdown<T> {
+    value: T;
+    selected: boolean;
+}
+
+const example: Dropdown<string> = {
+    value: 'ab',
+    selected: false,
+}
+
+
+// 제네릭의 타입 제한
+
+// T[] : 제네릭 타입이 무엇인지 타입스크립트에게 일종의 힌트를 주는 것. (제네릭의 타입 제한??)
+
+// function logTextLength<T>(text: T[]): T[] {
+//     console.log(text.length);
+//     text.forEach((text) => console.log(text));
+//     return text;
+// }
+
+
+// 제네릭 타입 제한 2 - 정의된 타입 이용하기
+interface LengthType {
+    length: number;
+}
+
+function logTextLength<T extends LengthType>(text: T):T {
+    text.length;
+    return text;
+}
+
+logTextLength('abac');
+
+
+// 제네릭 타입 제한 3 - keyof (예약어)를 이용해서
+interface ShoppingItem {
+    name: string;
+    price: number;
+    stock: number;
+}
+
+function getShoppingItemOption<T>(itemOption: T):T {
+    return itemOption;
+}
--- class-note/class.js
+++ class-note/class.js
@@ -1,3 +1,10 @@
+// function Person(name, age) {
+//     this.name = name;
+//     this.age = age;
+// }
+
+// 아래에 있는 코드와 완전히 같음.
+
 class Person {
   // 클래스 로직
   constructor(name, age) {
@@ -8,4 +15,6 @@ class Person {
 }
 
 new Person(); // 생성되었습니다. -> constructor 안에 있는 내용 실행됨.
-const hyeyeonkim = new Person("혜연", 30);
+const hyeyeonkim = new Person("혜연", 30); // 변수에 생성된 객체 담기.
+
+console.log(hyeyeonkim); // 클래스로 만들어진 객체가 뿅!
